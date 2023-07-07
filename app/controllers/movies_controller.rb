8add01ddba90fcb56f20ef7cea867e954dc5a5d5
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-07-07 10:32:27 -0600

    add movies#index action.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,3 +1,20 @@
 class MoviesController < ApplicationController
-  def index; end
+  def index
+    @keyword = params[:q]
+    @user = User.find(params[:user_id])
+    conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
+      faraday.params["api_key"] = ENV["TMDB_API_KEY"]
+    end
+    if @keyword == "top%20rated"
+      response = conn.get("https://api.themoviedb.org/3/movie/top_rated")
+    else
+      response = conn.get("https://api.themoviedb.org/3/search/movie?query=#{@keyword}")
+    end
+
+    json = JSON.parse(response.body, symbolize_names: true)
+    @movies = json[:results]
+  end
+  
+  def show; end
+  
 end
