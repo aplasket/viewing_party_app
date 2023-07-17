dd51f3c53e2cf8b52ed95096d1d1ff3acca54398
Ashley Plasket <ashley.plasket@gmail.com>
2023-07-17 17:43:34 -0400

    add addtl validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
-  validates :password, presence: true
+  validates :password, presence: true, confirmation: true
   validates :password_confirmation, presence: true
 
   has_secure_password
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe User, type: :model do
     it { should validate_uniqueness_of(:email).case_insensitive }
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:password) }
+    it { should validate_confirmation_of(:password) }
     it { should validate_presence_of(:password_confirmation) }
     it { should have_secure_password }
   end
