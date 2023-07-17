55231fa0be934854a77f7becb2f9062152905323
Ashley Plasket <ashley.plasket@gmail.com>
2023-07-17 14:43:54 -0400

    update to a movie object

--- spec/poros/movie_spec.rb
+++ spec/poros/movie_spec.rb
@@ -1,43 +1,24 @@
 require "rails_helper"
 
-RSpec.describe Movie do
+RSpec.describe Movie, :vcr do
   before(:each) do
-    data = {
-      id: 1,
-      title: "Newest Movie",
-      vote_average: 9.1,
-      genres: [
-        {
-          "id": 18,
-          "name": "Drama"
-        },
-        {
-          "id": 80,
-          "name": "Crime"
-        }
-      ],
-      overview: "a tale of a lame movie",
-      runtime: 189,
-      poster_path: "/movie/image.jpg"
-    }
-
-    @movie = Movie.new(data)
+    params = {id: 238}
+    @movie = MovieFacade.new(params).movie
   end
 
   it "exists and has attributes" do
     expect(@movie).to be_a(Movie)
     expect(@movie.id).to be_an(Integer)
-    expect(@movie.id).to eq(1)
+    expect(@movie.id).to eq(238)
     expect(@movie.title).to be_a(String)
-    expect(@movie.title).to eq("Newest Movie")
+    expect(@movie.title).to eq("The Godfather")
     expect(@movie.vote_average).to be_an(Float)
-    expect(@movie.vote_average).to eq(9.1)
+    expect(@movie.vote_average).to eq(8.711)
     expect(@movie.genres).to be_an(Array)
     expect(@movie.overview).to be_a(String)
-    expect(@movie.overview).to eq("a tale of a lame movie")
     expect(@movie.runtime).to be_an(Integer)
-    expect(@movie.runtime).to eq(189)
+    expect(@movie.runtime).to eq(175)
     expect(@movie.poster_path).to be_a(String)
-    expect(@movie.poster_path).to eq("/movie/image.jpg")
+    expect(@movie.poster_path).to eq("/3bhkrj58Vtu7enYsRolD1fZdja1.jpg")
   end
 end
