2283d8c823ce38709e5b0d058f79775a994ecba7
Ashley Plasket <ashley.plasket@gmail.com>
2023-07-18 20:10:14 -0400

    add password confirmation to factories.rb

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     name { Faker::Name.name }
     email { Faker::Internet.email(name: name, separators: ['-']) }
     password { Faker::Internet.password }
+    password_confirmation { password }
   end
 
   factory :party do
