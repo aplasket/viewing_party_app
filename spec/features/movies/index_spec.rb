95d9e7a37fc439a56dd4aacd01a4d8bad98bab93
Ashley Plasket <ashley.plasket@gmail.com>
2023-07-18 20:09:42 -0400

    update movies index spec with discover path and movies path

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -1,16 +1,23 @@
 require "rails_helper"
 
-RSpec.describe "/users/:id/movies", type: :feature do
+RSpec.describe "movies_path", type: :feature do
   let!(:user) { create(:user, password: "test123", password_confirmation: "test123") }
 
+  before(:each) do
+    visit login_path
+    fill_in :email, with: user.email
+    fill_in :password, with: user.password
+    click_on "Submit"
+  end
+  
   describe "when a user visits the discovery page" do
     describe "and they click on Find Top Rated Movies", :vcr do
       it "routes to a movie index page where the top movies are rendered" do
-        visit user_discover_index_path(user)
+        visit discover_path
 
         click_button("Find Top Rated Movies")
 
-        expect(current_path).to eq(user_movies_path(user))
+        expect(current_path).to eq(movies_path)
 
         expect(page).to_not have_content("Movie results for:")
 
@@ -26,14 +33,14 @@ RSpec.describe "/users/:id/movies", type: :feature do
 
     describe "they search for a movie in the search bar", :vcr do
       it "routes to the movie index page where search results are rendered" do
-        visit user_discover_index_path(user)
+        visit discover_path
 
         within ".search-movies" do
           fill_in "Search:", with: "Princess"
           click_button "Find Movies"
         end
 
-        expect(current_path).to eq(user_movies_path(user))
+        expect(current_path).to eq(movies_path)
 
         expect(page).to have_content("Movie results for: Princess")
         expect(page).to have_css(".movie", maximum: 20)
@@ -49,11 +56,11 @@ RSpec.describe "/users/:id/movies", type: :feature do
 
   describe "as a user on the movie index page" do
     it "displays a button to return back to the discover page", :vcr do
-      visit user_movies_path(user)
+      visit movies_path
 
       click_button "Discover Page"
 
-      expect(current_path).to eq(user_discover_index_path(user))
+      expect(current_path).to eq(discover_path)
     end
   end
 end
